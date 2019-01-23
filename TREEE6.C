b0bb6dc6d348e6a856f1e1317ad2fb537d46bf21
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-01-23 23:50:22 +0530

    Add files via upload

---
+++ TREEE6.C
@@ -0,0 +1,113 @@
+// Program to Search any Specific Node in a Binary Search Tree.
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+typedef struct node
+{
+	int data;
+	struct node *left;
+	struct node *right;
+}node;
+
+node *createNode(node *root, int);
+void traverse(node *root);
+int searchNode(node *root, int);
+
+void main()
+{
+	node *root=NULL;
+	int choice, n, x;
+
+	clrscr();
+
+	while(1)
+	{
+		printf("\n1. Press 1 to Create Node");
+		printf("\n2. Inorder Traversal");
+		printf("\n3. Search Node");
+		printf("\n4. Exit");
+
+		printf("\n\nEnter your Choice: ");
+		scanf("%d",&choice);
+
+		switch(choice)
+		{
+			case 1:
+				printf("\nEnter Data to Insert: ");
+				scanf("%d",&n);
+				root=createNode(root, n);
+				break;
+
+			case 2:
+				printf("\n");
+				traverse(root);
+				printf("\n");
+				break;
+
+			case 3:
+				printf("\nEnter Node Data to Search: ");
+				scanf("%d",&x);
+				x=searchNode(root, x);
+				if(x==0)
+					printf("\nNode not Found!\n");
+				else
+					printf("\nNode Exists\n");
+				break;
+
+			case 4:
+				exit(1);
+		}
+	}
+}
+node *createNode(node *root, int n)
+{
+	if(root==NULL)
+	{
+		root=(node *)malloc(sizeof(node));
+		root->data=n;
+		root->left=NULL;
+		root->right=NULL;
+	}
+	else if(n < root->data)
+	{
+		root->left=createNode(root->left, n);
+	}
+	else if(n > root->data)
+	{
+		root->right=createNode(root->right, n);
+	}
+	else
+	{
+		printf("\nDuplicate Nodes!");
+		delay(1000);
+		exit(1);
+	}
+	return root;
+}
+void traverse(node *root)
+{
+	if(root==NULL)
+	    return;
+	else
+	{
+		traverse(root->left);
+		printf("%d\t",root->data);
+		traverse(root->right);
+	}
+}
+int searchNode(node *root, int x)
+{
+	if(root==NULL)
+		return 0;
+
+	if(x==root->data)
+		return 1;
+
+	else if(x < root->data)
+		searchNode(root->left, x);
+
+	else if(x > root->data)
+		searchNode(root->right, x);
+}
