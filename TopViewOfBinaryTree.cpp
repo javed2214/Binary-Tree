8fb4809c54a5cb539cdfc0df16dc82dec8fed8ce
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-02-07 20:22:59 +0530

    Add files via upload

---
+++ TopViewOfBinaryTree.cpp
@@ -0,0 +1,79 @@
+// Program to Print Top View of a Binary Tree
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef struct node{
+	int data;
+	struct node *left;
+	struct node *right;
+}node;
+
+node *createTree(node *root, int n){
+	if(root==NULL){
+		root=(node *)malloc(sizeof(node));
+		root->data=n;
+		root->left=NULL;
+		root->right=NULL;
+		return root;
+	}
+	else if(n<root->data)
+		root->left=createTree(root->left, n);
+	else if(n>root->data)
+		root->right=createTree(root->right, n);
+}
+
+void printTree(node *root){
+	if(root){
+		printTree(root->left);
+		cout<<root->data<<" ";
+		printTree(root->right);
+	}
+}
+
+void getVerticalOrder(node *root, int hd, map<int,vector<int>>&Map){
+
+	if(root==NULL) return;
+	Map[hd].push_back(root->data);
+	getVerticalOrder(root->left,hd-1,Map);
+	getVerticalOrder(root->right,hd+1,Map);	
+}
+
+void TopView(node *root){
+
+	if(root==NULL) return;
+
+	queue<pair<node*,int>>q;
+	map<int,int>Map;
+	q.push(make_pair(root,0));
+	while(!q.empty()){
+		pair<node*,int>temp=q.front();
+		node *p=temp.first;
+		int hd=temp.second;
+		q.pop();
+		if(Map.find(hd)==Map.end()){
+			Map[hd]=p->data;
+			cout<<p->data<<" ";
+		}
+		if(p->left!=NULL)
+			q.push(make_pair(p->left,hd-1));
+		if(p->right!=NULL)
+			q.push(make_pair(p->right,hd+1));
+	}
+}
+
+int main(){	
+	int a[]={20,10,40,5,2,15,30,50,22,35,36,37};
+	int n=sizeof(a)/sizeof(int);
+
+	node *root=NULL;
+
+	for(int i=0;i<n;i++)
+		root=createTree(root,a[i]);
+
+	printTree(root);
+	cout<<"\n\nTop View: \n";
+	TopView(root);
+
+	return 0;
+}
