2292d4b82ff100688b3427fdff4700f9debb3942
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-07-28 12:59:34 +0530

    Update Iterative-Inorder-Traversal.cpp

--- Iterative-Inorder-Traversal.cpp
+++ Iterative-Inorder-Traversal.cpp
@@ -1,4 +1,8 @@
 // // https://www.interviewbit.com/problems/inorder-traversal/
+// Algorithm:-
+// 1. If node is not NULL, Push it into the Stack and Move to its Left Child
+// 2. If node is NULL Pop Element from the Stack and Move to its Right Child
+// 3. Else break
 
 vector<int> Solution::inorderTraversal(TreeNode* root) {
         
