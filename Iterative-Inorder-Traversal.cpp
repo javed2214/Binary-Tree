d36a8cef7cb6f4bf9773b5d616ca450ddae2f9e5
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-07-28 12:56:40 +0530

    Update Iterative-Inorder-Traversal.cpp

--- Iterative-Inorder-Traversal.cpp
+++ Iterative-Inorder-Traversal.cpp
@@ -1,55 +1,29 @@
-// Iterative Inorder Traversal of a Bianary Tree Using Stack
+// // https://www.interviewbit.com/problems/inorder-traversal/
 
-#include<bits/stdc++.h>
-using namespace std;
+vector<int> Solution::inorderTraversal(TreeNode* root) {
+        
+    stack <TreeNode *> S;
+    vector<int> v;
+    
+    TreeNode *p = root;
 
-typedef struct node{
-	int data;
-	struct node *left;
-	struct node *right;
-}node;
-
-node *newNode(int n){
-
-	node *root=new node;
-	root->data=n;
-	root->left=NULL;
-	root->right=NULL;
-
-	return root;
+    while(true){
+        
+        if(p!=NULL){
+            S.push(p);
+            p=p->left;
+        }
+        else{
+            if(!S.empty()){
+                p = S.top();
+                S.pop();
+                int vx = p->val;
+                v.push_back(vx);
+                p=p->right;
+                
+            }
+            else break;
+        }
+    }
+    return v;
 }
-
-void iterativeInorder(node *root){
-
-	if(root==NULL) return;
-
-	stack<node *> S;
-	node *p=root;
-
-	while(p!=NULL or !S.empty()){
-
-		while(p!=NULL){
-			S.push(p);
-			p=p->left;
-		}
-		p=S.top();
-		cout<<p->data<<" ";
-		S.pop();
-
-		p=p->right;
-	}
-}
-
-int main(){
-
-	node *root=newNode(1); 
-    root->left=newNode(2); 
-    root->right=newNode(3); 
-    root->left->left=newNode(4); 
-    root->left->right=newNode(5); 
-
-    iterativeInorder(root);
-
-    return 0;
-}
-
