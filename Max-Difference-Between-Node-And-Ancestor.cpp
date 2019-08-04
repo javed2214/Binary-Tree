19ba73566e134caa2d6ec0df7bfe807d492f3afa
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-08-04 13:00:15 +0530

    Add files via upload

---
+++ Max-Difference-Between-Node-And-Ancestor.cpp
@@ -0,0 +1,33 @@
+// Maximum Difference Between Node and Ancestor
+// https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/
+
+
+class Solution {
+public:
+    
+    int diff = INT_MIN;
+    
+    void getMax(TreeNode *root, int mx, int mi){
+        
+        if(root == NULL) return;
+       
+        mx = max(mx, root->val);
+        mi = min(mi, root->val);
+        
+        diff = max(diff, mx - mi);
+        
+        getMax(root->left,mx,mi);
+        getMax(root->right,mx,mi);
+    }
+    
+    int maxAncestorDiff(TreeNode* root) {
+        
+        if(!root) return 0;
+        
+        int mx = INT_MIN, mi = INT_MAX;
+
+        getMax(root,mx,mi);
+        
+        return diff;
+    }
+};
