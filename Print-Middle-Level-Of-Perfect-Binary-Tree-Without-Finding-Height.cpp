616db671c4244828c62dd507e21fcad7a88f9317
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-09-29 00:21:36 +0530

    Add files via upload

---
+++ Print-Middle-Level-Of-Perfect-Binary-Tree-Without-Finding-Height.cpp
@@ -0,0 +1,89 @@
+// Program to Print Middle Level of Binary Tree Without Finding Height
+// https://www.geeksforgeeks.org/print-middle-level-perfect-binary-tree-without-finding-height/
+
+/*
+
+The idea is similar to method 2 of finding middle of singly linked list.
+
+Use fast and slow (or tortoise) pointers in each route of a the tree.
+1. Advance fast pointer towards leaf by 2.
+2. Advance slow pointer towards lead by 1.
+3. If fast pointer reaches the leaf print value at slow pointer
+4. Call recursively the next route.
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef struct node{
+	int data;
+	struct node *left;
+	struct node *right;
+}node;
+
+node *newNode(int x){
+
+	node *temp = (node *)malloc(sizeof(node));
+	temp->data = x;
+	temp->left = NULL;
+	temp->right = NULL;
+
+	return temp;
+}
+
+void printMiddleLevel(node *fast, node *slow){
+
+	if(fast == NULL or slow == NULL) return;
+
+	if(fast->left == NULL and fast->left == NULL){
+		cout<<slow->data<<" ";
+		return;
+	}
+
+	printMiddleLevel(fast->left->left, slow->left);
+	printMiddleLevel(fast->right->right, slow->right);
+}
+
+int main(){
+
+	node *root = newNode(1); 
+  
+    root->left        = newNode(2); 
+    root->right       = newNode(3); 
+  
+    root->left->left  = newNode(4); 
+    root->left->right = newNode(5); 
+    root->right->left  = newNode(6); 
+    root->right->right = newNode(7); 
+  
+    root->left->left->left  = newNode(8); 
+    root->left->left->right  = newNode(9); 
+    root->left->right->left  = newNode(10); 
+    root->left->right->right  = newNode(11); 
+    root->right->left->left  = newNode(12); 
+    root->right->left->right  = newNode(13); 
+    root->right->right->left  = newNode(14); 
+    root->right->right->right  = newNode(15); 
+  
+    root->left->left->left->left  = newNode(16); 
+    root->left->left->left->right  = newNode(17); 
+    root->left->left->right->left  = newNode(18); 
+    root->left->left->right->right  = newNode(19); 
+    root->left->right->left->left  = newNode(20); 
+    root->left->right->left->right  = newNode(21); 
+    root->left->right->right->left  = newNode(22); 
+    root->left->right->right->right  = newNode(23); 
+    root->right->left->left->left  = newNode(24); 
+    root->right->left->left->right  = newNode(25); 
+    root->right->left->right->left  = newNode(26); 
+    root->right->left->right->right  = newNode(27); 
+    root->right->right->left->left  = newNode(28); 
+    root->right->right->left->right  = newNode(29); 
+    root->right->right->right->left  = newNode(30); 
+    root->right->right->right->right  = newNode(31); 
+
+    printMiddleLevel(root, root);
+
+    return 0;
+}
