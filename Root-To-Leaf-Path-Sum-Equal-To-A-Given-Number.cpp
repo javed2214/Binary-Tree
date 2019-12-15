b0dcfbf82985b617c2cbdfdbfe0af6b05baa46f5
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-12-15 19:43:33 +0530

    Update Root-To-Leaf-Path-Sum-Equal-To-A-Given-Number.cpp

--- Root-To-Leaf-Path-Sum-Equal-To-A-Given-Number.cpp
+++ Root-To-Leaf-Path-Sum-Equal-To-A-Given-Number.cpp
@@ -1,8 +1,11 @@
 // Root To Leaf Path Sum Equal to a Given Number
+// https://leetcode.com/problems/path-sum/
 
 // Given a binary tree and a number, return true if the tree has a root-to-leaf path such that adding 
 // up all the values along the path equals the given number.
 // Return false if no such path can be found.
+
+// ******************************** METHOD 1 ********************************
 
 bool hasPathSum(node *root, int sum){
 
@@ -10,3 +13,17 @@ bool hasPathSum(node *root, int sum){
 	if(!root->left and !root->right) return ((sum - root->data) == 0);
 	return (hasPathSum(root->left, sum - root->data) or hasPathSum(root->right, sum - root->data));
 }
+
+
+// ******************************** METHOD 2 ********************************
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        
+        if(!root) return false;
+        sum -= root->val;
+        if(!root->left and !root->right and !sum) return true;
+        return hasPathSum(root->left, sum) or hasPathSum(root->right, sum);
+    }
+};
